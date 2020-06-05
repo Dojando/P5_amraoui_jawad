0a9afe0b874237f436a98fe5210089f08f3ddf7d
Dojando <wadja.b@hotmail.fr>
2020-06-05 15:34:49 +0200

    mise en page dynamique des articles avec javascript / création de la page produit

---
+++ frontend/js/article.js
@@ -0,0 +1,100 @@
+// const container = document.getElementById('container');
+// console.log(container,"container");
+
+// container.innerHTML = `
+// <div class="col-12 col-md-6">
+// <article class="card">
+//      <img class=”card-img-top” src=”” alt=”...”>
+//      <div class="card-body">
+//          <h2>Produit</h2>
+//      </div>
+// </article>
+// </div>`;
+// let data = [];
+// let function = fetch('http://localhost:3000/api/cameras')
+// .then(function(response) {
+//     return response.json();
+// })
+// .then(function(json){
+//     // console.log(json);
+//     return json;
+//     }
+// })
+// .catch(function(error) {
+//     console.log('Il y a eu un problème avec l\'opération fetch: ' + error.message);
+// });
+// console.log(data);
+
+
+// const container = document.getElementById('container');
+// console.log(container,"container");
+
+// fetch('http://localhost:3000/api/cameras')
+// .then( response => response.json() )
+// .then( json => console.log(json[0].name) )
+// .then(function() {
+//     for (let i = 0; i < json.length; i++) {
+//         container.innerHTML =
+//         '<div class="col-12 col-md-6">'+
+//         '<article class="card">'+
+//             '<img class=”card-img-top” src='+json[i].imageUrl+' alt=”...”>'+
+//             '<div class="card-body">'+
+//                 '<h2>'+json[i].name+'</h2>'+
+//             '</div>'+
+//         '</article>'+
+//         '</div>';
+//     }
+// })
+// .catch(function() {
+//     console.log("y'a une erreur");
+// });
+
+
+const container = document.getElementById('container');
+console.log(container,"container");
+
+var request = new XMLHttpRequest();
+
+request.onreadystatechange = function() {
+    if (this.readyState == XMLHttpRequest.DONE && this.status == 200) {
+        var response = JSON.parse(this.responseText);
+        console.log(response[0].name);
+        for (let i = 0; i < response.length; i++)
+        container.innerHTML +=
+        '<div class="col-12 col-md-6">'+
+        '<article class="card">'+
+        '<img class=”card-img-top” src='+response[i].imageUrl+' alt=”...”>'+
+        '<div class="card-body">'+
+        '<h2>'+response[i].name+'</h2>'+
+        '</div>'+
+        '</article>'+
+        '</div>';
+    }
+};
+
+request.open("GET", "http://localhost:3000/api/cameras");
+request.send();
+
+
+// Recuperer les informations des articles (image et nom) dans des tableaux
+
+// Pour tous les articles :
+
+// injecter le code html d'un article avec les variables nom et image
+
+// Et ceux, tant que i < aux tableau.length
+
+
+
+// for (let i = 0; i < json.length; i++) {
+//     container.innerHTML = `
+// <div class="col-12 col-md-6">
+// <article class="card">
+//     <img class=”card-img-top” src=”+json[i].imageUrl+” alt=”...”>
+//     <div class="card-body">
+//         <h2>+json[i].name+</h2>
+//     </div>
+// </article>
+// </div>
+// `;
+// }
