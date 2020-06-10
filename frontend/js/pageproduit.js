fa1aa819a6d870308b36d9cd1a89136e6ddae5ac
Dojando <wadja.b@hotmail.fr>
2020-06-10 19:55:23 +0200

    creation du fichier js pour dynamiser la page produit et amélioration des autres éléments du site

---
+++ frontend/js/pageproduit.js
@@ -0,0 +1,12 @@
+// observer l'élément sur lequel j'ai cliqué sur la page articles
+
+// charger la page produit avec les informations du meme array que l'élément sur lequel j'ai cliqué
+
+
+let article = document.getElementsByClassName("lien_article");
+let nomArticle = document.querySelector('.lien_article h2');
+console.log(article);
+
+article.addEventListener('click', function() {
+
+}); 
