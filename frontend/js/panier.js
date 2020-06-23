f7ac09d1030f91128eacfe33c7c9042bea16cdab
Dojando <wadja.b@hotmail.fr>
2020-06-23 16:43:22 +0200

    création de la page de confirmation de commande / ajout du bouton 'panier' dans les headers / création d'un script pour la validation du formulaire du panier

--- frontend/js/panier.js
+++ frontend/js/panier.js
@@ -18,7 +18,8 @@ console.log(btnSupprimer.length);
 
 for (var i = 0; i < btnSupprimer.length; i++) {
     btnSupprimer[i].addEventListener('click' , function() {
-        Storage.removeItem(btnSupprimer[i].id);
+        console.log(btnSupprimer[0].id);
+        localStorage.removeItem(btnSupprimer[i].id);
     });
 }
 
@@ -46,3 +47,73 @@ for (let z = 0; z < listPrix.length; z++) {
     y = y + listPrix[z];
     prixTotal.innerHTML = y + ' EUR';
 }
+
+
+// script pour la validation du formulaire
+
+let regex_formulaire = {
+    prenom: /^[A-Z-]{1,100}$/i,
+    nom: /^[A-Z-]{1,100}$/i,
+    adresse: /^[A-Z-\d ]{1,100}$/i,
+    ville: /^[A-Z- ]{1,100}$/i,
+    email: /^([a-z\d\.-]+)@([a-z\d-]+)\.([a-z]+)$/i,
+}
+
+let form = document.getElementsByClassName('needs-validation');
+let prenom = document.getElementById('Prenom');
+let nom = document.getElementById('Nom');
+let adresse = document.getElementById('Adresse');
+let ville = document.getElementById('Ville');
+let email = document.getElementById('Email');
+let input = [prenom, nom, adresse, ville, email];
+
+let prenomValue;
+let nomValue;
+let adresseValue;
+let villeValue;
+let emailValue;
+let inputValue = [prenomValue, nomValue, adresseValue, villeValue, emailValue];
+
+form[0].addEventListener('submit', function(e) {
+    inputValue[0] = regex_formulaire.prenom.test(prenom.value);
+    inputValue[1] = regex_formulaire.nom.test(nom.value);
+    inputValue[2] = regex_formulaire.adresse.test(adresse.value);
+    inputValue[3] = regex_formulaire.ville.test(ville.value);
+    inputValue[4] = regex_formulaire.email.test(email.value);
+    for (let i = 0; i < input.length; i++) {
+        if (inputValue[i] == false) {
+            e.preventDefault();
+            input[i].classList.remove('is-valid');
+            input[i].classList.add('is-invalid');
+        } else {
+            input[i].classList.remove('is-invalid');
+            input[i].classList.add('is-valid');
+        }
+    }
+})
+
+
+
+
+// function ajouterPanier() {
+ 
+//     // Récupération des informations du panier
+//     let panier = JSON.parse(localStorage.getItem("panier")) || [];
+ 
+//     // La variable contenant les informations
+//     var informationsObjet = {
+//         "_id": url
+//     };
+ 
+//     // Si le panier est vide on stocke simplement les informations du produit dedans
+//     if (!panier) {
+//         localStorage.setItem("panier", [JSON.stringify(informationsObjet)]);
+//     }
+ 
+//     // Sinon on ajoute les informations du produit à celles déjà stockées
+//     else {
+//         panier.push(informationsObjet);
+//         localStorage.setItem("panier", [JSON.stringify(panier)]);
+//     }
+ 
+// }
